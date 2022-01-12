1c4f36e3afe3dc444eb461a97b1117ccc55c96d3
Jn <52610256+ododok@users.noreply.github.com>
2022-01-12 01:00:39 +0100

    How to scan char data type

---
+++ Baekjoon11654.java
@@ -0,0 +1,15 @@
+//https://www.acmicpc.net/problem/11654
+
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println((int)sc.next().charAt(0));
+
+    }
+}
